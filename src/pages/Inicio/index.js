db15f7a2715da7338f94dc4393ee36d07f352c47
Ivan Cruz <ivangtx19@gmail.com>
2024-07-30 13:09:56 -0600

    feat: Agrega funcionalidad de responsividad en todas las páginas -Refactoriza código para mejor legibilidad

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -4,6 +4,7 @@ import Card from "components/Card";
 import styles from "./index.module.css";
 import { useState, useEffect } from "react";
 
+
 function Inicio() {
   // Estado local para almacenar los videos obtenidos.
   const [videos, setVideos] = useState([]);
@@ -21,16 +22,21 @@ function Inicio() {
 
   return (
     <>
-      <Banner img="home" color="#154580" /> {/* Muestra el componente Banner con la imagen "home" y color de fondo "#154580". */}
-      <Titulo>
-        <h1>Un lugar para guardar sus videos favoritos </h1> {/* Muestra el título de la página. */}
-      </Titulo>
+      <main className={styles.mainInicio}>
+        <Banner  img="home" color="#154580" /> {/* Muestra el componente Banner con la imagen "home" y color de fondo "#154580". */}
+        <Titulo>
+          <h1>Un lugar para guardar sus videos favoritos </h1> {/* Muestra el título de la página. */}
+        </Titulo>
 
-      <section className={styles.container}> {/* Contenedor principal para los videos con estilos del módulo CSS. */}
-        {videos.map((video) => { // Itera sobre el array de videos.
-          return <Card {...video} key={video.id} />; // Renderiza un componente Card para cada video, pasando todas las propiedades del video.
-        })}
-      </section>
+        <section className={styles.container}> {/* Contenedor principal para los videos con estilos del módulo CSS. */}
+          {videos.map((video) => { // Itera sobre el array de videos.
+            return <Card {...video} key={video.id} />; // Renderiza un componente Card para cada video, pasando todas las propiedades del video.
+          })}
+        </section>
+      </main>
+
+
+
     </>
   );
 }
--- src/pages/PaginaBase/index.js
+++ src/pages/PaginaBase/index.js
@@ -1,5 +1,4 @@
 import Cabecera from "components/Cabecera/Cabecera"
-import Container from "components/Container"
 import FavoritoProvider from "context/Favoritos"
 import Pie from "components/Pie"
 import { Outlet } from "react-router-dom"
@@ -9,9 +8,7 @@ function PaginaBase() {
         <main>
             <Cabecera/> {/* Muestra la cabecera en la parte superior de la página */}
             <FavoritoProvider> {/* Proveedor del contexto de favoritos, disponible para los componentes hijos */}
-                <Container> {/* Contenedor para el contenido principal */}
                     <Outlet/> {/* Renderiza el contenido de las rutas hijas aquí */}
-                </Container>
             </FavoritoProvider>
             <Pie/> {/* Muestra el pie de página en la parte inferior de la página */}
         </main>
